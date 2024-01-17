13101e0f37f07bb9565610468e3ab517a9418fc5
Ashish Sawant <ashishsawant9075@gmail.com>
2024-01-17 22:41:34 +0530

    Commiting changes

--- src/app/service/data-service.service.ts
+++ src/app/service/data-service.service.ts
@@ -48,8 +48,8 @@ export class DataServiceService {
 
 
   constructor(private http: HttpClient) {
-    // this.userUrl = 'http://localhost:8080/always-noted'
-    this.userUrl = 'http://16.171.5.124:8080/notes'
+     this.userUrl = 'http://localhost:8080/always-noted'
+   // this.userUrl = 'http://16.171.5.124:8080/notes'
 
   }
 
